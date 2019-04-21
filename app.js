b8b186a35f79c63b954ee9d573a6e30409495d24
davidbet2 <davidbet2@hotmail.com>
2019-04-21 18:57:50 -0500

    Cors

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var bodyParser = require('body-parser');
 var app = express();
 var fileUpload = require('express-fileupload');
 
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS ")
+    next();
+});
+
+
 // Body parser
 
 // Body Parser
